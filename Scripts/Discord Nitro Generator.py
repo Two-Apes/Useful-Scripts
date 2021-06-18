0f51ad216849e3f1dd05b1699d9f1c168b05a5d5
Andrei <78364462+Wtf-Is-This-x1337@users.noreply.github.com>
2021-06-18 08:27:27 -1000

    Update Discord Nitro Generator.py

--- Scripts/Discord Nitro Generator.py
+++ Scripts/Discord Nitro Generator.py
@@ -1,5 +1,5 @@
 import random, string
-amount = int(input('TWO-APES | (all the codes are unchecked) How many Nitro codes do you want to be generated?: '))
+amount = int(input('! (all the codes are unchecked) How many Nitro codes do you want to be generated?: '))
 value = 1
 while value <= amount:
     code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
